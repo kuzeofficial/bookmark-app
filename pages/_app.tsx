76ce787e19765ab95bf4bf6b05200247ce5744d0
Cristian Fonseca <cfonsecacomas@gmail.com>
2021-11-10 02:44:28 -0300

    :tada:

---
+++ pages/_app.tsx
@@ -0,0 +1,11 @@
+// Styles
+import '../styles/base.css'
+// Import Types 
+import type { AppProps } from 'next/app'
+
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
